08441f17414eccbc7ca066ef4069a57f4643ee66
owefsad <dongzhiyong@secnium.cn>
2021-09-18 13:58:17 +0800

    fix service status default data

---
+++ mysql/update-20210918.sql
@@ -0,0 +1,14 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Records of engine_monitoring_indicators
+-- ----------------------------
+BEGIN;
+INSERT INTO `engine_monitoring_indicators` VALUES (1, 'dongtai-method-pool-scan', '漏洞扫描任务', 'Vul Scan Task', '漏洞扫描队列');
+INSERT INTO `engine_monitoring_indicators` VALUES (2, 'periodic_task', '定时任务', 'Schedule Task', '定时任务');
+INSERT INTO `engine_monitoring_indicators` VALUES (3, 'dongtai-search-scan', '策略更新任务', 'Sink Update Task', '策略更新任务');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
