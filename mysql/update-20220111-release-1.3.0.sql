e20be9c0780855e662145cb9f32c6ab13f8e1aeb
xianzelin@huoxian.cn <xianzelin@huoxian.cn>
2022-01-24 19:20:32 +0800

    hotfix:add-JNDI-injection

--- mysql/update-20220111-release-1.3.0.sql
+++ mysql/update-20220111-release-1.3.0.sql
@@ -16155,6 +16155,8 @@ FROM
 WHERE
         type = 4;
 
+INSERT IGNORE INTO iast_strategy (user_id, vul_type, level_id, state, dt, vul_name, vul_desc, vul_fix, hook_type_id, vul_desc_en, vul_desc_zh, vul_fix_en, vul_fix_zh, vul_name_en, vul_name_zh) VALUES(1, 'JNDI注入', 1, 'enable', 1641449965, 'JNDI注入', 'JNDI注入', '禁用lookup方法或禁用远程协议加载', NULL, NULL, 'JNDI注入', NULL, '禁用lookup方法或禁用远程协议加载', NULL, 'JNDI注入');
+
 CREATE TABLE IF NOT EXISTS `iast_project_header` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) NOT NULL,
