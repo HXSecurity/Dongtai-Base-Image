3b642a292e6dab8d3e7fcd5ffc74998471bc3721
xianzelin@huoxian.cn <xianzelin@huoxian.cn>
2022-02-08 16:03:30 +0800

    mysqltest

---
+++ test/hook_strategy_export.py
@@ -0,0 +1,125 @@
+######################################################################
+# @author      : bidaya0 (bidaya0@$HOSTNAME)
+# @file        : hook_strategy_export
+# @created     : 星期一 2月 07, 2022 16:29:01 CST
+#
+# @description :
+######################################################################
+
+from test_strategy import TargetDB
+
+cursor = TargetDB.db.cursor()
+hook_type_query_sql = '''
+select * from iast_hook_type iht 
+where id in (select hooktype_id from iast_hook_strategy_type ihst 
+where  hooktype_id in (
+select id from iast_hook_type iht where  language_id  = 1 AND enable in (0,1) and `type` in (1,2,3,4) )
+and hookstrategy_id  in (select id from iast_hook_strategy ihs where created_by = 1 and enable in (0,1)));
+'''
+
+hook_type_sql = '''
+INSERT IGNORE INTO `iast_hook_type` (`type`, `name`, `value`, `create_time`
+	, `update_time`, `created_by`, `enable`, `name_en`, `name_zh`
+	, `language_id`)
+SELECT %s, %s, %s, %s
+	, %s, %s, %s, %s, %s
+	, %s FROM DUAL WHERE NOT EXISTS (SELECT `id` FROM iast_hook_type WHERE 
+`type`=%s AND `name`= %s
+AND  value = %s AND create_time= %s AND
+update_time = %s  AND `created_by`= %s
+AND enable = %s AND name_en = %s AND name_zh = %s AND language_id = %s LIMIT 1);
+SET @HOOK_TYPE_ID = (SELECT `id` FROM iast_hook_type WHERE 
+`type`=%s AND `name`= %s
+AND  value = %s AND create_time = %s AND
+update_time = %s  AND `created_by` = %s
+AND enable = %s AND name_en = %s AND name_zh = %s AND language_id = %s LIMIT 1);
+'''
+
+hook_strategy_query_sql = '''
+select * from iast_hook_strategy ihs2 
+where id in (select hookstrategy_id from iast_hook_strategy_type ihst 
+where  hooktype_id in (
+select id from iast_hook_type iht where  language_id  = 1 AND enable in (0,1) and `type` in (1,2,3,4))
+and hookstrategy_id  in (select id from iast_hook_strategy ihs where created_by = 1 and enable in (0,1)));
+'''
+
+hook_strategy_sql = '''
+INSERT IGNORE INTO iast_hook_strategy
+(value, source, target, inherit, track, create_time, update_time, created_by, enable)
+SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s FROM DUAL 
+WHERE NOT EXISTS (SELECT `id` FROM iast_hook_strategy WHERE 
+`value` = %s AND `source` = %s AND `target` = %s AND `inherit` = %s AND `track` = %s AND `create_time` = %s AND `update_time` = %s AND `created_by` = %s AND `enable` = %s LIMIT 1);
+SET @IAST_HOOK_STRATEGY_ID = (SELECT `id` FROM iast_hook_strategy WHERE 
+`value` = %s AND `source` = %s AND `target` = %s AND `inherit` = %s AND `track` = %s AND `create_time` = %s AND `update_time` = %s AND `created_by` = %s AND `enable` = %s LIMIT 1);
+'''
+
+hook_strategy_type_query_sql = '''
+select * from iast_hook_strategy_type ihst
+where  hooktype_id in (
+select id from iast_hook_type iht where  language_id  = 1 AND enable in (0,1) and `type` in (1,2,3,4))
+and hookstrategy_id  in (select id from iast_hook_strategy ihs where created_by = 1 and enable in (0,1));
+'''
+
+finsql = hook_strategy_sql.format(
+    **{
+        i: '%s'
+        for i in [
+            'value', 'source', 'target', 'inherit', 'track', 'create_time',
+            'update_time', 'created_by', 'enable'
+        ]
+    })
+
+
+cursor.execute(hook_type_query_sql)
+
+
+def escape_null_equal(sql):
+    return sql.replace('= NULL', 'IS NULL')
+
+
+hook_type_dict = {}
+for data in cursor.fetchall():
+    final_sql = cursor.mogrify(hook_type_sql, [
+        data['type'],
+        data['name'],
+        data['value'],
+        data['create_time'],
+        data['update_time'],
+        data['created_by'],
+        data['enable'],
+        data['name_en'],
+        data['name_zh'],
+        data['language_id'],
+    ] * 3)
+    hook_type_dict[int(data['id'])] = escape_null_equal(final_sql)
+
+
+
+
+cursor.execute(hook_strategy_query_sql)
+
+
+def escape_null_equal(sql):
+    return sql.replace('= NULL', 'IS NULL')
+
+
+hook_strategy_dict = {}
+for data in cursor.fetchall():
+    final_sql = cursor.mogrify(hook_strategy_sql, [
+        data[i] for i in [
+            'value', 'source', 'target', 'inherit', 'track', 'create_time',
+            'update_time', 'created_by', 'enable'
+        ]
+        ] * 3)
+    hook_strategy_dict[int(data['id'])] = escape_null_equal(final_sql)
+
+
+cursor.execute(hook_strategy_type_query_sql)
+for data in cursor.fetchall():
+    print(hook_type_dict[data['hooktype_id']])
+    print(hook_strategy_dict[data['hookstrategy_id']])
+    print(
+        'INSERT INTO iast_hook_strategy_type (hookstrategy_id, hooktype_id) VALUES (@IAST_HOOK_STRATEGY_ID, @HOOK_TYPE_ID);'
+    )
+
+#print(hook_strategy_dict)
