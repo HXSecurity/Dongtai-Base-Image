2cc5268030055b83f49b940e6165f5ab5c950d85
bidaya0 <bidaya0@bidaya0-x230.xianzelin.top>
2022-02-26 17:53:18 +0800

    fix vul_name and vul_type

---
+++ mysql/update-20220218-release-1.3.1-ENTERPRISE.sql
@@ -0,0 +1,7 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS=0;
+
+UPDATE IGNORE iast_strategy SET vul_name_zh='Response Without Content-Security-Policy Header' WHERE vul_type ='Response Without Content-Security-Policy Header';
+
+
+SET FOREIGN_KEY_CHECKS=1;
