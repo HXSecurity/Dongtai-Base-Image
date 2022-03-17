987ca55e3e67d1530cf27ffa2d6344504403a8c4
cm <songjinghao2012@163.com>
2022-03-17 20:49:53 +0800

    add agent config sql

--- mysql/update-20220316-release-1.4.0.sql
+++ mysql/update-20220316-release-1.4.0.sql
@@ -20,21 +20,50 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for iast_agent_config
 -- ----------------------------
-DROP TABLE IF EXISTS `iast_agent_config`;
-CREATE TABLE `iast_agent_config` (
+
+CREATE TABLE IF NOT EXISTS `iast_agent_config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL COMMENT '用户id',
   `details` json DEFAULT NULL,
   `hostname` varchar(100) DEFAULT NULL,
   `ip` varchar(100) DEFAULT NULL,
   `port` int(50) DEFAULT NULL,
-  `cluster_name` varchar(255) DEFAULT NULL COMMENT '容器名称',
-  `cluster_version` varchar(100) DEFAULT NULL COMMENT '容器版本号',
+  `cluster_name` varchar(255) DEFAULT NULL COMMENT '集群名称',
+  `cluster_version` varchar(100) DEFAULT NULL COMMENT '集群版本号',
   `priority` int(10) DEFAULT NULL COMMENT '优先级100>10',
   `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+  KEY `user_id` (`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- Table structure for iast_agent_upload_type_url
+-- ----------------------------
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+CREATE TABLE IF NOT EXISTS `iast_agent_upload_type_url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `type_id` int(5) DEFAULT NULL COMMENT '需要转发数据类型',
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '数据转发地址',
+  `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
+  `headers` json DEFAULT NULL COMMENT '请求头',
+  `send_num` int(11) DEFAULT '0' COMMENT '转发次数',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for iast_server
+-- ----------------------------
+ALTER TABLE iast_server ADD cluster_name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '集群名称';
+ALTER TABLE iast_server ADD cluster_version varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '集群名称';
+
 
 SET FOREIGN_KEY_CHECKS = 1;
