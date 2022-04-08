53893a30f1f540978f77367fa6d3017c603335af
Bidaya0 <xianzelin@huoxian.cn>
2022-04-08 10:35:40 +0800

    Update update-20220407-release-1.5.0.sql

--- mysql/update-20220407-release-1.5.0.sql
+++ mysql/update-20220407-release-1.5.0.sql
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS `iast_third_party_service` (
   PRIMARY KEY (`id`)
 )
 
-ALTER TABLE iast_agent ADD filepathsimhash varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' NOT NULL COMMENT 'agent目录结构相似度哈希';
+ALTER TABLE iast_agent ADD filepathsimhash varchar(255)  DEFAULT '' NOT NULL COMMENT 'agent目录结构相似度哈希';
 ALTER TABLE iast_third_party_service ADD CONSTRAINT iast_third_party_service_UN UNIQUE KEY (address,service_type,port,agent_id);
-ALTER TABLE iast_agent ADD servicetype varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' NOT NULL COMMENT '部署的方式';
-CREATE INDEX iast_vulnerability_level_id_IDX USING BTREE ON iast_webapi_buss_test.iast_vulnerability (level_id,agent_id);
+ALTER TABLE iast_agent ADD servicetype varchar(255)  DEFAULT '' NOT NULL COMMENT '部署的方式';
+CREATE INDEX iast_vulnerability_level_id_IDX USING BTREE ON iast_vulnerability (level_id,agent_id);
 
 SET FOREIGN_KEY_CHECKS=1;
