6e1bc6de9ebdfcbd799aadbc18d02e74f950b796
bidaya0 <bidaya0@bidaya0-x230.xianzelin.top>
2022-04-16 23:42:47 +0800

    bugfix:1.6.0

--- mysql/update-20220407-release-1.6.0.sql
+++ mysql/update-20220407-release-1.6.0.sql
@@ -21,7 +21,7 @@ CREATE TABLE `iast_vul_integration` (
   KEY `iast_agent_config_1_user_id_IDX` (`user_id`,`vul_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE iast_webapi_buss_test.iast_agent_config_1 (
+CREATE TABLE iast_agent_config_1 (
 	id int(11) auto_increment NOT NULL,
 	user_id int(11) NULL COMMENT '用户id',
 	vul_id int(11) NULL COMMENT '漏洞id',
@@ -41,8 +41,21 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci
 COMMENT='';
-CREATE INDEX user_id USING BTREE ON iast_webapi_buss_test.iast_agent_config_1 (user_id);
-CREATE INDEX iast_agent_config_1_user_id_IDX USING BTREE ON iast_webapi_buss_test.iast_agent_config_1 (user_id,vul_id);
+CREATE INDEX user_id USING BTREE ON iast_agent_config_1 (user_id);
+CREATE INDEX iast_agent_config_1_user_id_IDX USING BTREE ON iast_agent_config_1 (user_id,vul_id);
+
+INSERT INTO web_url_route (id, `path`, name, component, meta_keepAlive, meta_disabled, meta_i18n, meta_isMenu, parent, meta_name, redirect, name_i18n_zh, name_i18n_en, name_i18n) VALUES(42, 'integration', 'integration', 'views/setting/integration.vue', 'False', 'False', 'menu.integration', 'False', 6, 'menu.integration', '', '', '', '');
+INSERT INTO web_url_route (id, `path`, name, component, meta_keepAlive, meta_disabled, meta_i18n, meta_isMenu, parent, meta_name, redirect, name_i18n_zh, name_i18n_en, name_i18n) VALUES(43, 'jira', 'jira', 'views/setting/jira.vue', 'False', 'True', 'menu.jira', 'False', 6, 'menu.jira', '', '', '', '');
+INSERT INTO web_url_route (id, `path`, name, component, meta_keepAlive, meta_disabled, meta_i18n, meta_isMenu, parent, meta_name, redirect, name_i18n_zh, name_i18n_en, name_i18n) VALUES(44, 'chandao', 'chandao', 'views/setting/chandao.vue', 'False', 'True', 'menu.chandao', 'False', 6, 'menu.chandao', '', '', '', '');
+INSERT INTO web_url_route (id, `path`, name, component, meta_keepAlive, meta_disabled, meta_i18n, meta_isMenu, parent, meta_name, redirect, name_i18n_zh, name_i18n_en, name_i18n) VALUES(45, 'gitlab', 'gitlab', 'views/setting/gitlab.vue', 'False', 'True', 'menu.gitlab', 'False', 6, 'menu.gitlab', '', '', '', '');
+INSERT INTO web_url_route (id, `path`, name, component, meta_keepAlive, meta_disabled, meta_i18n, meta_isMenu, parent, meta_name, redirect, name_i18n_zh, name_i18n_en, name_i18n) VALUES(46, 'jenkins', 'jenkins', 'views/setting/jenkins.vue', 'False', 'True', 'menu.jenkins', 'False', 6, 'menu.jenkins', '', '', '', '');
+INSERT INTO web_role_url_relation (role_id, url_id) VALUES(6, 42);
+INSERT INTO web_role_url_relation (role_id, url_id) VALUES(6, 43);
+INSERT INTO web_role_url_relation (role_id, url_id) VALUES(6, 44);
+INSERT INTO web_role_url_relation (role_id, url_id) VALUES(6, 45);
+INSERT INTO web_role_url_relation (role_id, url_id) VALUES(6, 46);
+
+
 
 
 SET FOREIGN_KEY_CHECKS=1;
