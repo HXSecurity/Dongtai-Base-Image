45b19ad6f1dcb4f0ec1d95c9ea3415ef03333772
tscuite <tscuite@qq.com>
2022-06-24 21:05:34 +0800

    fix: 1.8.1

---
+++ mysql/update-20220607-release-1.8.1-2.sql
@@ -0,0 +1,425 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS=0;
+
+INSERT INTO `web_role` (`id`, `name`, `state`) VALUES (8, '收到发生的', 0);
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 226;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 227;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 228;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 229;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 230;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 231;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 232;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 233;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 234;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 235;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 236;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 237;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 256;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 257;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 258;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 259;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 260;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 261;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 262;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 263;
+
+DELETE FROM `web_role_button_relation` WHERE `id` = 264;
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (274, 7, 1);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (275, 7, 13);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (276, 7, 11);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (277, 7, 2);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (278, 7, 3);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (279, 7, 8);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (280, 7, 9);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (281, 7, 4);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (282, 7, 7);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (343, 6, 14);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (344, 6, 15);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (345, 6, 1);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (346, 6, 13);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (347, 6, 11);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (348, 6, 2);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (349, 6, 3);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (350, 6, 8);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (351, 6, 9);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (352, 6, 4);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (353, 6, 7);
+
+INSERT INTO `web_role_button_relation` (`id`, `role_id`, `button_id`) VALUES (354, 6, 6);
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1602;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1603;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1604;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1605;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1606;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1607;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1608;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1609;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1610;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1611;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1612;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1613;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1614;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1615;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1616;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1617;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1618;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1619;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1620;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1621;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1622;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1623;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1624;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1625;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1626;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1627;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1628;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1629;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1630;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1631;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1632;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1633;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1634;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1635;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1636;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1637;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1638;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1639;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1640;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1641;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1642;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1643;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1644;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1645;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1646;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1647;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1708;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1709;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1710;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1711;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1712;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1713;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1714;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1715;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1716;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1717;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1718;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1719;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1720;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1721;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1722;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1723;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1724;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1725;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1726;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1727;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1728;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1729;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1730;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1731;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1732;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1733;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1734;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1735;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1736;
+
+DELETE FROM `web_role_url_relation` WHERE `id` = 1737;
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1738, 8, 2);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1764, 7, 1);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1765, 7, 2);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1766, 7, 3);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1767, 7, 4);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1768, 7, 5);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1769, 7, 6);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1770, 7, 9);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1771, 7, 10);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1772, 7, 11);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1773, 7, 12);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1774, 7, 13);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1775, 7, 14);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1776, 7, 15);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1777, 7, 17);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1778, 7, 18);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1779, 7, 19);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1780, 7, 20);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1781, 7, 21);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1782, 7, 22);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1783, 7, 24);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1784, 7, 26);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1785, 7, 34);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1786, 7, 42);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1787, 7, 48);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1788, 7, 49);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (1789, 7, 50);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2036, 6, 1);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2037, 6, 2);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2038, 6, 3);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2039, 6, 4);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2040, 6, 5);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2041, 6, 6);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2042, 6, 7);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2043, 6, 8);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2044, 6, 9);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2045, 6, 10);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2046, 6, 11);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2047, 6, 12);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2048, 6, 13);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2049, 6, 14);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2050, 6, 15);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2051, 6, 17);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2052, 6, 18);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2053, 6, 19);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2054, 6, 20);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2055, 6, 21);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2056, 6, 22);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2057, 6, 23);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2058, 6, 24);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2059, 6, 26);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2060, 6, 27);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2061, 6, 28);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2062, 6, 29);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2063, 6, 30);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2064, 6, 33);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2065, 6, 34);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2066, 6, 35);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2067, 6, 36);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2068, 6, 37);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2069, 6, 38);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2070, 6, 39);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2071, 6, 41);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2072, 6, 42);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2073, 6, 43);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2074, 6, 44);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2075, 6, 45);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2076, 6, 46);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2077, 6, 47);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2078, 6, 48);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2079, 6, 49);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2080, 6, 50);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2081, 6, 51);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2082, 6, 53);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2083, 6, 54);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2084, 6, 55);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2085, 6, 56);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2086, 6, 57);
+
+INSERT INTO `web_role_url_relation` (`id`, `role_id`, `url_id`) VALUES (2087, 6, 58);
+
+INSERT INTO `web_role_user_relation` (`id`, `role_id`, `user_id`) VALUES (38, 7, 580);
+
+INSERT INTO `web_role_user_relation` (`id`, `role_id`, `user_id`) VALUES (41, 7, 581);
+
+INSERT INTO `web_role_user_relation` (`id`, `role_id`, `user_id`) VALUES (42, 7, 304);
+
+INSERT INTO `web_role_user_relation` (`id`, `role_id`, `user_id`) VALUES (44, 6, 583);
+
+INSERT INTO `web_url_route` (`id`, `path`, `name`, `component`, `meta_keepAlive`, `meta_disabled`, `meta_i18n`, `meta_isMenu`, `parent`, `meta_name`, `redirect`, `name_i18n_zh`, `name_i18n_en`, `name_i18n`) VALUES (53, 'reportCenter', 'reportCenter', 'views/project/reportCenter.vue', '', 'True', 'menu.reportCenter', '', 0, '', '', '', '', '');
+
+INSERT INTO `web_url_route` (`id`, `path`, `name`, `component`, `meta_keepAlive`, `meta_disabled`, `meta_i18n`, `meta_isMenu`, `parent`, `meta_name`, `redirect`, `name_i18n_zh`, `name_i18n_en`, `name_i18n`) VALUES (54, 'projectExport/:pid?', 'projectExport/:pid?', 'views/project/ProjectExport.vue', '', 'True', 'menu.projectExport', '', 1, '', '', '', '', '');
+
+INSERT INTO `web_url_route` (`id`, `path`, `name`, `component`, `meta_keepAlive`, `meta_disabled`, `meta_i18n`, `meta_isMenu`, `parent`, `meta_name`, `redirect`, `name_i18n_zh`, `name_i18n_en`, `name_i18n`) VALUES (55, 'kubernetes', 'kubernetes', 'views/setting/k8s.vue', 'False', 'True', 'menu.kubernetes', 'False', 6, 'menu.kubernetes', '', '', '', '');
+
+INSERT INTO `web_url_route` (`id`, `path`, `name`, `component`, `meta_keepAlive`, `meta_disabled`, `meta_i18n`, `meta_isMenu`, `parent`, `meta_name`, `redirect`, `name_i18n_zh`, `name_i18n_en`, `name_i18n`) VALUES (56, 'yunxiao', 'yunxiao', 'views/setting/yunxiao.vue', 'False', 'True', 'menu.yunxiao', 'False', 6, 'menu.yunxiao', '', '', '', '');
+
+INSERT INTO `web_url_route` (`id`, `path`, `name`, `component`, `meta_keepAlive`, `meta_disabled`, `meta_i18n`, `meta_isMenu`, `parent`, `meta_name`, `redirect`, `name_i18n_zh`, `name_i18n_en`, `name_i18n`) VALUES (57, 'IntelliJ', 'IntelliJ', 'views/setting/intelliJ.vue', 'False', 'True', 'menu.intelliJ', 'False', 6, 'menu.intelliJ', '', '', '', '');
+
+INSERT INTO `web_url_route` (`id`, `path`, `name`, `component`, `meta_keepAlive`, `meta_disabled`, `meta_i18n`, `meta_isMenu`, `parent`, `meta_name`, `redirect`, `name_i18n_zh`, `name_i18n_en`, `name_i18n`) VALUES (58, 'lanjing', 'lanjing', 'views/setting/lanjing.vue', 'False', 'True', 'menu.lanjing', 'False', 6, 'menu.lanjing', '', '', '', '');
+
+
+
+
+update `iast_strategy` set vul_desc="SSRF(Server-Side Request Forgery:服务器端请求伪造) 是一种由攻击者构造，由服务端发起请求的一个网络攻击，一般用来在外网探测或攻击内网服务，其影响效果根据服务器用的函数不同，从而造成不同的影响。" where id=3;
+
+SET FOREIGN_KEY_CHECKS = 1;
