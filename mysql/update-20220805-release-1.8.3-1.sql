8773078f5c5007fd59cc3e8212921798e7fe19a1
tscuite <tscuite@qq.com>
2022-08-09 18:24:45 +0800

    fix: 1.8.3

--- mysql/update-20220805-release-1.8.3-1.sql
+++ mysql/update-20220805-release-1.8.3-1.sql
@@ -47,4 +47,36 @@ ALTER TABLE `iast_vulnerability` ADD FULLTEXT INDEX `ft_index_search_keywords`(`
 ALTER TABLE `iast_vulnerability` ADD FULLTEXT INDEX `ft_index_vul_title`(`vul_title`);
 ALTER TABLE `iast_vulnerability` ADD FULLTEXT INDEX `ft_index_search`(`search_keywords`, `uri`, `vul_title`, `http_method`, `http_protocol`, `top_stack`, `bottom_stack`);
 
+
+UPDATE iast_asset_vul SET sid=NULL WHERE sid = '';
+
+DELETE  FROM iast_asset_vul 
+WHERE SID IN ( SELECT sid FROM (
+SELECT sid,COUNT(id) AS count_sid FROM iast_asset_vul iav 
+WHERE  sid  IS NOT NULL 
+GROUP BY sid 
+HAVING  count_sid > 1)tmp );
+
+DELETE FROM iast_asset_vul_type_relation 
+WHERE   (asset_vul_id ,asset_vul_type_id) IN (
+SELECT asset_vul_id ,asset_vul_type_id FROM
+(
+SELECT asset_vul_id ,asset_vul_type_id, count(id) AS _count  FROM iast_asset_vul_type_relation iavtr 
+GROUP BY    asset_vul_id ,asset_vul_type_id
+HAVING _count > 1 ) tmp);
+
+DELETE FROM iast_asset_vul_relation 
+WHERE asset_vul_id IN (
+SELECT id FROM iast_asset_vul iav2 
+WHERE SID IN ( SELECT sid FROM (
+SELECT sid,COUNT(id) AS count_sid FROM iast_asset_vul iav 
+WHERE  sid  IS NOT NULL 
+GROUP BY sid 
+HAVING  count_sid > 1)tmp ) );
+
+ALTER TABLE iast_asset DROP KEY signature_value;
+ALTER TABLE iast_asset_vul_relation ADD CONSTRAINT iast_asset_vul_relation_UN UNIQUE KEY (asset_id,asset_vul_id);
+ALTER TABLE iast_asset_vul_type_relation ADD CONSTRAINT iast_asset_vul_type_relation_UN UNIQUE KEY (asset_vul_id,asset_vul_type_id);
+ALTER TABLE iast_asset_vul ADD CONSTRAINT iast_asset_vul_UN UNIQUE KEY (sid);
+
 SET FOREIGN_KEY_CHECKS=1;
