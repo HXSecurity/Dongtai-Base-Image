93005fc5911b94554b54037118360820863fd258
Bidaya0 <xianzelin@huoxian.cn>
2022-11-08 15:30:48 +0800

    Create update-20221024-release-1.8.8.sql

---
+++ mysql/update-20221024-release-1.8.8.sql
@@ -0,0 +1,21 @@
+-- dongtai_webapi.iast_agent_black_rule definition
+
+CREATE TABLE `iast_agent_black_rule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `scope` int(11) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- dongtai_webapi.iast_agent_black_rule_detail definition
+
+CREATE TABLE `iast_agent_black_rule_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `update_time` datetime(6) NOT NULL,
+  `value` varchar(512) NOT NULL,
+  `rule_id` int(11) DEFAULT NULL,
+  `create_time` datetime(6) NOT NULL,
+  `operator` int(11) DEFAULT NULL,
+  `target_type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
