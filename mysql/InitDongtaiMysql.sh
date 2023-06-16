26cf4e3520ac297d2c8c2ec663cad6c84c873f10
tscuite <tscuite@qq.com>
2023-06-16 15:41:01 +0800

    feat: add sh

--- mysql/InitDongtaiMysql.sh
+++ mysql/InitDongtaiMysql.sh
@@ -2,14 +2,14 @@
 # 下载数据库文件
 # wget https://huoqi-public.oss-cn-beijing.aliyuncs.com/iast/sql/db.sql
 
-echo "#! /bin/bash" > mysqlBuild.sh
-echo "wget https://huoqi-public.oss-cn-beijing.aliyuncs.com/iast/sql/db.sql" >> mysqlBuild.sh
-cat updaterecord.txt | while read -r line;
+echo "#! /bin/bash" > mysql/mysqlBuild.sh
+echo "wget https://huoqi-public.oss-cn-beijing.aliyuncs.com/iast/sql/db.sql" >> mysql/mysqlBuild.sh
+cat mysql/updaterecord.txt | while read -r line;
 do
 if [[ $line == "" ]]; then
     continue
 fi
     PODNAME=`echo $line | awk '{print $2}'`;
     echo $PODNAME
-    echo "wget https://huoqi-public.oss-cn-beijing.aliyuncs.com/iast/sql/${PODNAME}" >> mysqlBuild.sh
+    echo "wget https://huoqi-public.oss-cn-beijing.aliyuncs.com/iast/sql/${PODNAME}" >> mysql/mysqlBuild.sh
 done
