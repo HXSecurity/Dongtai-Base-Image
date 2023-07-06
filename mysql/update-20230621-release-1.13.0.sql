af8e600b6fc59d057aaaf4ff3e61689d87eebcdd
Bidaya0 <xianzelin@huoxian.cn>
2023-07-06 16:56:15 +0800

    Update update-20230621-release-1.13.0.sql

--- mysql/update-20230621-release-1.13.0.sql
+++ mysql/update-20230621-release-1.13.0.sql
@@ -4,27 +4,31 @@ SET FOREIGN_KEY_CHECKS=0;
   
 -- dongtai_webapi.iast_api_route_v2 definition
 CREATE TABLE `iast_api_route_v2` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`path` varchar(255) NOT NULL,
-`method` varchar(31) NOT NULL,
-`from_where` int(11) NOT NULL DEFAULT '1' COMMENT '1-agent,2-method_pool',
-`project_id` int(11) NOT NULL DEFAULT '0' COMMENT '项目ID',
-`project_version_id` int(11) NOT NULL DEFAULT '0' COMMENT '项目版本ID',
-`is_cover` int(11) NOT NULL DEFAULT '0' COMMENT '是否覆盖',
-`api_info` json DEFAULT NULL COMMENT 'API接口的相关信息',
-PRIMARY KEY (`id`),
-UNIQUE KEY `iast_api_route_v2_UN` (`project_id`,`project_version_id`,`path`,`method`,`from_where`),
-KEY `iast_api_route_method_id_7ff9eb8e` (`method`) USING BTREE,
-KEY `iast_api_route_method_id_IDX` (`method`,`project_id`,`project_version_id`) USING BTREE,
-KEY `iast_api_route_path_IDX` (`method`,`path`,`project_id`,`project_version_id`) USING BTREE,
-KEY `iast_api_route_project_id_IDX` (`project_id`,`project_version_id`,`from_where`,`is_cover`,`method`,`path`) USING BTREE,
-KEY `iast_api_route_project_id_IDX_temp` (`project_id`,`project_version_id`,`method`,`path`) USING BTREE,
-KEY `iast_api_route_project_id_is_cover_IDX` (`project_id`,`project_version_id`,`is_cover`,`method`) USING BTREE,
-KEY `iast_api_route_project_version_id_IDX` (`project_version_id`,`method`,`path`) USING BTREE
+  `id` int(11) NOT NULL AUTO_INCREMENT,	
+  `path` varchar(255) NOT NULL,
+  `method` varchar(31) NOT NULL,
+  `from_where` int(11) NOT NULL DEFAULT '1' COMMENT '1-agent,2-method_pool',
+  `project_id` int(11) NOT NULL DEFAULT '0' COMMENT '项目ID',
+  `project_version_id` int(11) NOT NULL DEFAULT '0' COMMENT '项目版本ID',
+  `is_cover` int(11) NOT NULL DEFAULT '0' COMMENT '是否覆盖',
+  `api_info` json DEFAULT NULL COMMENT '接口的相关信息',
+  `create_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `iast_api_route_v2_UN` (`project_id`,`project_version_id`,`path`,`method`,`from_where`),
+  KEY `iast_api_route_method_id_7ff9eb8e` (`method`) USING BTREE,
+  KEY `iast_api_route_method_id_IDX` (`method`,`project_id`,`project_version_id`) USING BTREE,
+  KEY `iast_api_route_path_IDX` (`method`,`path`,`project_id`,`project_version_id`) USING BTREE,
+  KEY `iast_api_route_project_id_IDX` (`project_id`,`project_version_id`,`from_where`,`is_cover`,`method`,`path`) USING BTREE,
+  KEY `iast_api_route_project_id_IDX_temp` (`project_id`,`project_version_id`,`method`,`path`) USING BTREE,
+  KEY `iast_api_route_project_id_is_cover_IDX` (`project_id`,`project_version_id`,`is_cover`,`method`) USING BTREE,
+  KEY `iast_api_route_project_version_id_IDX` (`project_version_id`,`method`,`path`) USING BTREE
 ) ENGINE=InnoDB ;
 
 
-SET FOREIGN_KEY_CHECKS=1;
+ALTER TABLE iast_request_chains_vul_context ADD url varchar(2000) DEFAULT '' NULL;
+ALTER TABLE iast_request_chains_vul_context ADD request text NULL;
+ALTER TABLE iast_request_chains_vul_context ADD response text NULL;
 
 -- dongtai_engine.plugins.project_status definition
 alter table iast_project
@@ -86,5 +90,33 @@ WHERE t.id = 1;
 UPDATE iast_vulnerability t
 SET t.status_id = 1
 WHERE t.status_id IN (2, 4, 7);
+
+
+
+INSERT INTO web_url_route
+(id, `path`, name, component, meta_keepAlive, meta_disabled, meta_i18n, meta_isMenu, parent, meta_name, redirect, name_i18n_zh, name_i18n_en, name_i18n)
+VALUES(63, 'vulnSharing', 'vulnSharing', 'views/records/vulnSharing.vue', '', 'True', '', '', 0, '漏洞分享记录', '', '', '', '');
+INSERT INTO web_button
+(id, name, webroute_id)
+VALUES(49, '组件漏洞分享', 62);
+INSERT INTO web_button
+(id, name, webroute_id)
+VALUES(48, '详情漏洞分享', 13);
+INSERT INTO web_button
+(id, name, webroute_id)
+VALUES(47, '漏洞分享', 12);
+INSERT INTO web_role_button_relation
+(role_id, button_id)
+VALUES(6, 47);
+INSERT INTO web_role_button_relation
+(role_id, button_id)
+VALUES(6, 48);
+INSERT INTO web_role_button_relation
+(role_id, button_id)
+VALUES(6, 49);
+INSERT INTO web_role_url_relation
+(role_id, url_id)
+VALUES(6, 63);
+
 
 SET FOREIGN_KEY_CHECKS=1;
